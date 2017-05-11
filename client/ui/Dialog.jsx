dbedbdc9d858c7798db546254ef5c9ca1db85066
Jim Fulton <jim@jimfulton.info>
2017-05-11 15:14:18 -0400

    some abstraction around the react-toolbox dialog
    To avoid lots of boilerplate

---
+++ client/ui/Dialog.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {Dialog, Input} from 'react-toolbox';
+
+class Dialog_ extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { active: false };
+  }
+
+  show() {
+    this.setState({active: true});
+  }
+
+  hide() {
+    this.setState({active: false});
+  }
+
+  finish() {
+    if (! this.props.finish()) {
+      this.hide();
+    }
+  }
+
+  render() {
+    const cancel = () => this.hide();
+    const actions = [{label: "Cancel", onClick: cancel},
+                     {label: this.props.action || "Ok",
+                      onClick: () => this.finish()
+                     }];
+    return (
+      <Dialog
+         actions={actions}
+         active={this.state.active}
+         onEscDown={cancel}
+         onOverlayClick={cancel}
+         title={this.props.title}
+         type={this.props.type || "normal"}
+         >
+        {this.props.children}
+      </Dialog>
+    );
+  }
+}
+
+class Input_ extends React.Component {
+
+  render() {
+    return (
+      <Input {...this.props} value={this.props.onChange()} />
+    );
+  }
+}
+
+class DialogBase extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  val(name, default_='') {
+    return (v) => {
+      if (v == undefined) {
+        return this.state[name] || default_;
+      }
+      else {
+        const state = {};
+        state[name] = v;
+        return this.setState(state);
+      }
+    };
+  }
+
+  show(state) {
+    if (state) {
+      this.setState(state);
+    }
+    else {
+      this.state = {};
+      this.setState(this.state);
+    }
+    this.refs.dialog.show();
+  }
+}
+
+module.exports = {
+  Dialog: Dialog_,
+  Input: Input_,
+  DialogBase: DialogBase
+};
