0712d53c736247cd8b2e8a0aaffb896084e28db5
Jim Fulton <jim@jimfulton.info>
2017-05-31 20:34:07 -0400

    Added a user component and renamed and improved the avatar component
    The avatar component now provides a tooltip when a title is given.

--- client/ui/who.jsx
+++ client/ui/who.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {Avatar, Tooltip} from 'react-toolbox';
 import md5 from 'md5';
 
-module.exports = {
-  Gravitar: (props) => {
-    console.log(props.email);
-    const src = 'https://www.gravatar.com/avatar/' +
-            md5(props.email) + '.jpg?s=40&d=wavatar';
-    return <img src={src} />;
+const TAvatar = Tooltip(Avatar);
+
+const UserAvatar = (props) => {
+  const {email, size, title} = props;
+  const src = 'https://www.gravatar.com/avatar/' +
+          md5(email) + '.jpg?s=' + (size || 32) + '&d=wavatar';
+  if (title) {
+    return <TAvatar tooltip={title} image={src} />;
   }
+  else {
+    return <Avatar image={src} />;
+  }
+}
+
+module.exports = {
+  UserAvatar: UserAvatar,
+  User: (props) => (
+      <div className="kb-user">
+        <UserAvatar email={props.user.email} />
+        <div>
+          <div><strong>{props.user.name}</strong></div>
+          <div><small>{props.user.nick} {props.user.email}</small></div>
+        </div>
+      </div>
+  )
 };
