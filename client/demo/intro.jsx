44f470f88c82f77c71b761318b81fdfc42212228
Jim Fulton <jim@jimfulton.info>
2017-06-15 17:43:40 -0400

    Had to copy intro.jsx rather than use a link because webpack followed the link when looking for intro.html.

--- client/demo/intro.jsx
+++ client/demo/intro.jsx
@@ -1,1 +1,11 @@
-../ui/intro.jsx
+import React from 'react';
+
+import intro_html from './intro.html';
+
+const inner_html = {__html: intro_html};
+
+module.exports = 
+  (props) => (
+    <div className="kb-intro" dangerouslySetInnerHTML={inner_html}>
+    </div>
+  );
