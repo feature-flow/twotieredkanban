5dc737ae8e44c66bf384af593b17d5be50ff21ff
Jim Fulton <jim@jimfulton.info>
2017-06-25 11:59:54 -0400

    formatting

--- client/ui/boards.jsx
+++ client/ui/boards.jsx
@@ -14,10 +14,10 @@ module.exports = {
       if (boards.length > 0) {
         return boards.map((board) => {
           return (
-              <ListItem
-                caption={board.name}
-                onClick={() => goto_board(board)}
-                key={board.name}
+            <ListItem
+               caption={board.name}
+               onClick={() => goto_board(board)}
+              key={board.name}
               />
           );
         });
@@ -36,16 +36,17 @@ module.exports = {
     
   },
   AddBoardDialog: class extends DialogBase {
-  render() {
-    return (
-      <Dialog
-         title="New Board" action="Add" ref="dialog"
-         finish={() => this.props.api.add_board(this.state.name)}
-        >
-        <Input label='Name' required={true} onChange={this.required("name")} />
-      </Dialog>
-    );
+    render() {
+      return (
+        <Dialog
+           title="New Board" action="Add" ref="dialog"
+           finish={() => this.props.api.add_board(this.state.name)}
+          >
+          <Input label='Name' required={true}
+                 onChange={this.required("name")} />
+        </Dialog>
+      );
+    }
   }
-}
 
 };
